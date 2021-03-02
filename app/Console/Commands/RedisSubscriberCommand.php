70162ba31d7ef14abebe5753537189b7e56b2569
Temitope Olotin <olotintemitope@gmail.com>
2021-03-02 08:29:49 +0100

    get and post response to the webhook and queue it up

--- app/Console/Commands/RedisSubscriberCommand.php
+++ app/Console/Commands/RedisSubscriberCommand.php
@@ -2,12 +2,27 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Repository\TopicRepository;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Redis;
 use RedisException;
+use Spatie\WebhookServer\WebhookCall;
 
 class RedisSubscriberCommand extends Command
 {
+    /**
+     * @var TopicRepository
+     */
+    private $repository;
+
+    public function __construct(TopicRepository $repository)
+    {
+        parent::__construct();
+
+        $this->repository = $repository;
+    }
+
     /**
      * The name and signature of the console command.
      *
@@ -29,11 +44,36 @@ class RedisSubscriberCommand extends Command
     public function handle()
     {
         try {
-            Redis::subscribe(['topic1'], function ($message) {
-                echo $message;
+            $eventName = 'topic1';
+
+            $topic = $this->repository->slug($eventName);
+            $subscribers = $topic->subscribers;
+
+            Redis::subscribe([$eventName], function ($payload) use ($topic, $subscribers) {
+                foreach ($this->getOptimizedSubscribers($subscribers) as $url) {
+                    WebhookCall::create()
+                        ->url($url)
+                        ->payload([
+                            'topic' => $topic->slug,
+                            'data' => json_decode($payload, true, 512, JSON_THROW_ON_ERROR)
+                        ])
+                        ->useSecret('sampler_must_be_great')
+                        ->dispatch();
+                }
             });
-        } catch (RedisException $exception) {
+        } catch (RedisException | \JsonException $exception) {
             echo $exception->getMessage();
+        }
+    }
+
+    /**
+     * @param Collection $subscribers
+     * @return \Generator
+     */
+    protected function getOptimizedSubscribers(Collection $subscribers): \Generator
+    {
+        foreach ($subscribers as $subscriber) {
+            yield $subscriber->url;
         }
     }
 }
